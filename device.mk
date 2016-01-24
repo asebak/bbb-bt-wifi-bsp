9c4ddf9601ee569f78901987de346c44cd68ef06
Ahmad Sebak <Ahmad.Sebak@Honeywell.com>
2016-01-23 16:04:19 -0800

    modifications to bsp for wifi and bt dongles

---
+++ device.mk
@@ -0,0 +1,117 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+PRODUCT_COPY_FILES := \
+	device/ti/beagleboneblack/init.genericam33xx_flatteneddevicetr.rc:root/init.genericam33xx\(flatteneddevicetr.rc \
+	device/ti/beagleboneblack/init.genericam33xx_flatteneddevicetr.usb.rc:root/init.genericam33xx\(flatteneddevicetr.usb.rc \
+	device/ti/beagleboneblack/vold.fstab:system/etc/vold.fstab \
+	device/ti/beagleboneblack/fstab.genericam33xx_flatteneddevicetr:root/fstab.genericam33xx\(flatteneddevicetr \
+	device/ti/beagleboneblack/ueventd.genericam33xx_flatteneddevicetr.rc:root/ueventd.genericam33xx\(flatteneddevicetr.rc \
+	device/ti/beagleboneblack/media_codecs.xml:system/etc/media_codecs.xml \
+	device/ti/beagleboneblack/media_profiles.xml:system/etc/media_profiles.xml \
+	device/ti/beagleboneblack/mixer_paths.xml:system/etc/mixer_paths.xml \
+	device/ti/beagleboneblack/audio_policy.conf:system/etc/audio_policy.conf
+
+# Bluetooth
+PRODUCT_COPY_FILES += \
+        frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+		frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
+
+# Wifi
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	device/ti/beagleboneblack/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+
+# KeyPads
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/ti-tsc.idc:system/usr/idc/ti-tsc.idc
+
+# BBBAndroid - Since the users keep asking for GPIO key mappings for the
+# various LCDs (which enumerate in a variety of values), I'm just going
+# to copy links to the base one a bunch of times and hope for the best.
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gpio_keys_10.kl:system/usr/keylayout/gpio_keys_10.kl \
+    $(LOCAL_PATH)/gpio_keys_11.kl:system/usr/keylayout/gpio_keys_11.kl \
+    $(LOCAL_PATH)/gpio_keys_12.kl:system/usr/keylayout/gpio_keys_12.kl \
+    $(LOCAL_PATH)/gpio_keys_13.kl:system/usr/keylayout/gpio_keys_13.kl \
+    $(LOCAL_PATH)/gpio_keys_14.kl:system/usr/keylayout/gpio_keys_14.kl \
+    $(LOCAL_PATH)/gpio_keys_15.kl:system/usr/keylayout/gpio_keys_15.kl \
+    $(LOCAL_PATH)/gpio_keys_16.kl:system/usr/keylayout/gpio_keys_16.kl
+
+PRODUCT_PROPERTY_OVERRIDES := \
+       hwui.render_dirty_regions=false
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/main.nonsmartphone.conf:system/etc/bluetooth/main.conf  
+	   
+# Explicitly specify dpi, otherwise the icons don't show up correctly with SGX enabled
+PRODUCT_PROPERTY_OVERRIDES += \
+       wifi.interface=wlan0 \
+       wlan.driver.status=ok \
+       ro.sf.lcd_density=160
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.sys.strictmode.visual=0 \
+	persist.sys.strictmode.disable=1
+
+PRODUCT_CHARACTERISTICS := tablet,nosdcard
+
+DEVICE_PACKAGE_OVERLAYS := \
+    device/ti/beagleboneblack/overlay
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+PRODUCT_PACKAGES += \
+	librs_jni \
+	com.android.future.usb.accessory
+
+PRODUCT_PACKAGES += \
+	libaudioutils
+
+PRODUCT_PACKAGES += \
+        audio.primary.beagleboneblack \
+        tinycap \
+        tinymix \
+        tinyplay
+
+PRODUCT_PACKAGES += \
+	dhcpcd.conf
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+	make_ext4fs
+
+# Backlight HAL (liblights)
+PRODUCT_PACKAGES += \
+	lights.beagleboneblack
+
+PRODUCT_PACKAGES += \
+	FileManager-1.1.6
+
+PRODUCT_PACKAGES += \
+	androidvncserver
+
+PRODUCT_PACKAGES += \
+	camera.omap3
+
+# BBBAndroid - Additional external packages
+PRODUCT_PACKAGES += \
+        i2c-tools \
+        dropbear
+
+$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
---
+++ patches/patch.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Execute this shell script to apply various patches to the Android
+# codebase. Be sure to execute the script from the root of the 
+# Android codebase (where patch.sh is located).
+
+# CallStack backwards compatibility with SGX binary blob 
+# From Chris Simmonds (csimmonds@2net.co.uk)  
+cd system/core
+patch -p1 < ../../device/ti/beagleboneblack/patches/0001-Fix-CallStack-API.patch
+cd ../..
+
+# Bionic patch to fix nlink type
+# https://android.googlesource.com/platform/bionic.git/+/ff5b0fc8f3406c98561e17bd2a6c5433e192f5be%5E1..ff5b0fc8f3406c98561e17bd2a6c5433e192f5be/
+cd bionic
+patch -p1 < ../device/ti/beagleboneblack/patches/0001-Fix-nlink-type.patch
+cd ../
+
+# Remove emulator check for bluetooth since we need qemu=1
+cd frameworks/base 
+patch -p1 < ../../device/ti/beagleboneblack/patches/SystemServer.java.patch
+cd ../..
+
+# Show battery icon as always powered
+cd system/core
+patch -p1 < ../../device/ti/beagleboneblack/patches/0008-ENGR00289825-Make-simulated-battery-status-report-ba.patch
+cd ../..
+
+# Patch kernel defaults
+cd kernel
+patch -p1 <../device/ti/beagleboneblack/patches/am335x_evm_android_defconfig.patch
+cd ..
